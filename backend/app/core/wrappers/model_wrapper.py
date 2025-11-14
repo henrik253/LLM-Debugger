e93bb72a79ac0ecedc76268605d33eba6c774e63
Henrik Floeter <floe9894>
2025-11-14 00:37:16 +0000

    Add gemma3

--- backend/app/core/wrappers/model_wrapper.py
+++ backend/app/core/wrappers/model_wrapper.py
@@ -1,53 +1,45 @@
 from abc import ABC, abstractmethod  # Import abstract base class utilities
-from layer_type import LayerType
 
 # Base class for model wrappers that define a common interface for interacting with models
 class ModelWrapper(ABC):
     def __init__(self, model): 
         self.model = model  # Store the model instance
         self.current_timestep = 0  # Keep track of the current timestep
-
+        self.model_output = ''
+        self.current_model_output = ''
+        self.current_prompt = '' 
+        
     @abstractmethod
     def get_network_output(self, prompt: str): 
         pass  # Return the model output for a given text prompt
-    
+
     @abstractmethod
-    def get_summarized_layers_with_ids(self) -> dict[list[int],LayerType]:
-        pass  # Return a list describing the types of layers in the model
-    
+    def get_network_architecture(self): 
+      pass
+
     @abstractmethod
-    def get_layer_activations(self, layer_index: int): 
+    def get_layer_activations(self, layer_index: int, layer_name : str): 
         pass  # Return the activations of a specific layer
     
     @abstractmethod
-    def set_layer_activation(self, layer_index: int, neuron_index: int):
+    def set_layer_activation(self, layer_index: int, layer_name : str, neuron_index: int):
         pass  # Manually set the activation value of a specific neuron
     
     @abstractmethod
-    def get_layer_input_param_avgs(self, layer_index: int):
+    def get_layer_input_param_avgs(self, layer_index: int, layer_name : str):
         pass  # Return average input parameter values for a given layer
     
     @abstractmethod
-    def get_layer_input_param_stds(self, layer_index: int):
+    def get_layer_input_param_stds(self, layer_index: int, layer_name : str):
         pass  # Return standard deviations of input parameters for a given layer
     
     @abstractmethod
-    def get_layer_param(self, layer_index: int, neuron_index: int, input_index: int): 
+    def get_layer_param(self, layer_index: int, layer_name : str, source_index : int,target_index : int): 
         pass  # Get a specific parameter (e.g., weight) of a neuron input connection
     
-    @abstractmethod
-    def set_layer_param(self, layer_index: int, neuron_index: int, input_index: int, param_value: float): 
-        pass  # Set a specific parameter (e.g., weight) to a new value
-
     # Set the timestep from which the model information should be retrieved
     def set_timestep(self, index: int):
         self.current_timestep = index
 
     def layer_has_param(layer):
         pass
-
-# Abstract representation of a layer
-class LayerWrapper: 
-    def __init__(self,layer_type : LayerType = None,real_indeces : list = None):
-        self.layer_type = layer_type
-        self.real_indeces = real_indeces 
--- backend/app/utils/layer_type.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum 
-
-class LayerType(Enum): 
-
-    POSITIONAL_ENCODING = 'Positional Encoding Layer'
-    MHSA = 'Multi-Head Self-Attention'
-    ATTENTION_HEAD = 'Attention Head'
-    FFN = 'Feed Forward'
-    RESIDUAL = 'Skip Connection'
